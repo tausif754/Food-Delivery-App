0cd7271d22b78276d59242fc3ea911811f9943f7
Tausif Ahmad <tausifahmad754@gmail.com>
2024-03-16 23:59:38 +0530

    pages are created

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.png";
 import basket_icon from "../../assets/basket_icon.png";
 const Navbar = () => {
+  const [menu, setMenu] = useState("Home");
   return (
     <div className="navbar">
       <img src={logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-App</li>
-        <li>Contact Us</li>
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("Mobile-App")}
+          className={menu === "Mobile-App" ? "active" : ""}
+        >
+          Mobile-App
+        </li>
+        <li
+          onClick={() => setMenu("Contact Us")}
+          className={menu === "Contact Us" ? "active" : ""}
+        >
+          Contact Us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={search_icon} alt="" />
