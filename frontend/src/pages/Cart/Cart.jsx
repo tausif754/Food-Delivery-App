5b84cb68365269342f510afa278787b76396093a
Tausif Ahmad <tausifahmad754@gmail.com>
2024-03-26 23:28:01 +0530

    In add to cart section css applied

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useState(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return (
     <div className="cart">
       <div className="cart-items">
@@ -21,7 +21,12 @@ const Cart = () => {
           if (cartItems[item._id] > 0) {
             return (
               <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
                 <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price * cartItems[item._id]}</p>
+                <p>x</p>
               </div>
             );
           }
