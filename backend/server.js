f8610ac79b2598a8660fc065bcbe280eb6336b21
Tausif Ahmad <tausifahmad754@gmail.com>
2024-05-07 17:03:31 +0530

    Database conneted successfully

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+// app config
+const app = express();
+const port = 4000;
+
+// db connection
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+app.listen(port, () => {
+  console.log(`Sarver started on http://localhost:${port}`);
+});
