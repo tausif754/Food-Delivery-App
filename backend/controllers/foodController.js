c7e06e7f90be6dee1ce16a9bbe3e14c8e1ccfd86
Tausif Ahmad <tausifahmad754@gmail.com>
2024-05-09 12:17:09 +0530

    Created food remove API

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -21,4 +21,30 @@ const addFood = async (req, res) => {
     res.json({ success: false, message: Error });
   }
 };
-export { addFood };
+
+// all food list *its not work
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "error" });
+  }
+};
+
+//remove food item
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food Removed" });
+  } catch (error) {
+    console.log("error");
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood, listFood, removeFood };
